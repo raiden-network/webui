a58069ac99196265acfe7d69bb8bf218f0f510e1
Konstantinos Paparas <kelsos86@gmail.com>
2018-12-07 17:43:32 +0100

    Adds karma-junit-reporter for ci statistics

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
+            require('karma-junit-reporter'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
@@ -22,7 +23,7 @@ module.exports = function (config) {
         angularCli: {
             environment: 'dev'
         },
-        reporters: ['progress', 'kjhtml'],
+        reporters: ['progress', 'kjhtml', 'junit'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
@@ -34,6 +35,11 @@ module.exports = function (config) {
                 flags: ['--no-sandbox', '--disable-gpu']
             }
         },
-        singleRun: false
+        singleRun: false,
+        junitReporter: {
+            outputDir: 'test-reports',
+            outputFile: 'test-results.xml',
+            userBrowserName: false
+        }
     });
 };
