350e8fa2aa2de42a379a25e6ef92eccb818896dd
Konstantinos Paparas <kelsos86@gmail.com>
2019-01-07 16:57:55 +0100

    Add a temporary workaround for web3 cors on firefox

--- src/app/services/raiden.config.ts
+++ src/app/services/raiden.config.ts
@@ -5,6 +5,7 @@ import { SharedService } from './shared.service';
 // @ts-ignore
 import * as Web3 from 'web3';
 import { BatchManager } from './batch-manager';
+import { HttpProvider, Provider } from 'web3/providers';
 
 interface RDNConfig {
     raiden: string;
@@ -29,6 +30,10 @@ const default_config: RDNConfig = {
     reveal_timeout: 10,
     environment_type: EnvironmentType.DEVELOPMENT
 };
+
+function override(object, methodName, callback) {
+    object[methodName] = callback(object[methodName]);
+}
 
 @Injectable()
 export class RaidenConfig {
@@ -56,26 +61,54 @@ export class RaidenConfig {
                     this.api = this.config.raiden;
                     this.sharedService.httpTimeout = this.config.http_timeout;
 
-                    this.web3 = new Web3(new Web3.providers.HttpProvider(this.config.web3, 2000));
+                    this.web3 = new Web3(this.provider(2000));
                     // make a simple test call to web3
 
                     this.web3.eth.net.getId().catch(reason => {
                         console.error(`Invalid web3 endpoint, switching to fallback ${this.config.web3_fallback}`, reason);
                         this.config.web3 = this.config.web3_fallback;
-                        this.web3 = new Web3(new Web3.providers.HttpProvider(this.config.web3));
+                        this.web3 = new Web3(this.provider());
                         this.createBatchManager();
                         reject(reason);
                     }).then(() => {
-                        this.web3 = new Web3(new Web3.providers.HttpProvider(this.config.web3));
+                        this.web3 = new Web3(this.provider());
                         this.createBatchManager();
                         resolve();
                     });
-
                 });
         });
     }
 
+    private provider(timeout?: number): Provider {
+        return this.monkeyPatchProvider(new Web3.providers.HttpProvider(this.config.web3, timeout));
+    }
+
     private createBatchManager() {
         this._batchManager = new BatchManager(this.web3.currentProvider);
+    }
+
+    // TODO: Workaround for https://github.com/ethereum/web3.js/issues/1803 it should be immediately removed
+    // as soon as the issue is fixed upstream.
+    // Issue is also documented here https://github.com/ethereum/web3.js/issues/1802
+    private monkeyPatchProvider(httpProvider: HttpProvider) {
+        override(httpProvider, '_prepareRequest', function () {
+            return function () {
+                const request = new XMLHttpRequest();
+
+                request.open('POST', this.host, true);
+                request.setRequestHeader('Content-Type', 'application/json');
+                request.timeout = this.timeout && this.timeout !== 1 ? this.timeout : 0;
+
+                if (this.headers) {
+                    this.headers.forEach(function (header) {
+                        request.setRequestHeader(header.name, header.value);
+                    });
+                }
+
+                return request;
+            };
+        });
+
+        return httpProvider;
     }
 }
