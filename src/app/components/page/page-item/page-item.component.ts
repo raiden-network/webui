04ae4ebfd05524b309cf2a9be95bf24bca009193
Konstantinos Paparas <kelsos86@gmail.com>
2019-03-04 18:10:46 +0100

    Makes page item to move buttons depending on the layout

--- src/app/components/page/page-item/page-item.component.ts
+++ src/app/components/page/page-item/page-item.component.ts
@@ -5,17 +5,22 @@ import {
     ViewEncapsulation
 } from '@angular/core';
 import { Animations } from '../../../animations/animations';
+import { MediaObserver } from '@angular/flex-layout';
 
 @Component({
     selector: 'app-page-item',
     templateUrl: './page-item.component.html',
-    styleUrls: ['./page-item.component.css'],
+    styleUrls: ['./page-item.component.scss'],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
     animations: Animations.flyout
 })
 export class PageItemComponent implements OnInit {
-    constructor() {}
+    constructor(private mediaObserver: MediaObserver) {}
 
     ngOnInit() {}
+
+    isMobile(): boolean {
+        return this.mediaObserver.isActive('xs');
+    }
 }
