d90058b95731f7865eb444f26a2bf15ecf32b7c2
Konstantinos Paparas <kelsos86@gmail.com>
2019-03-05 15:29:14 +0100

    Makes the license page display properly on mobile

--- src/app/components/license/license.component.ts
+++ src/app/components/license/license.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-license',
     templateUrl: './license.component.html',
-    styleUrls: ['./license.component.css']
+    styleUrls: ['./license.component.scss']
 })
 export class LicenseComponent implements OnInit {
     constructor() {}
