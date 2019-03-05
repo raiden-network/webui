306837028995f4d796423c2cbfc27a767703a6fd
Konstantinos Paparas <kelsos86@gmail.com>
2019-03-05 19:15:26 +0100

    Adds more tests

--- src/app/components/add-address-dialog/add-address-dialog.component.spec.ts
+++ src/app/components/add-address-dialog/add-address-dialog.component.spec.ts
@@ -9,16 +9,41 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SharedService } from '../../services/shared.service';
 import { LocalStorageAdapter } from '../../adapters/local-storage-adapter';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { clickElement, mockInput } from '../../../testing/interaction-helper';
+import Spy = jasmine.Spy;
+
+// noinspection Angular2DeclarationMembershipInModule
+@Component({
+    template: `
+        <app-add-address-dialog></app-add-address-dialog>
+    `
+})
+class TestHostComponent {}
 
 describe('AddAddressDialogComponent', () => {
-    let component: AddAddressDialogComponent;
-    let fixture: ComponentFixture<AddAddressDialogComponent>;
+    let component: TestHostComponent;
+    let fixture: ComponentFixture<TestHostComponent>;
+    let spy: Spy;
+
+    function getDebugElement() {
+        return fixture.debugElement.query(
+            By.directive(AddAddressDialogComponent)
+        );
+    }
 
     beforeEach(async(() => {
+        const spyObj = jasmine.createSpyObj('MatDialogRef', ['close']);
+        spy = spyObj.close;
         TestBed.configureTestingModule({
-            declarations: [AddAddressDialogComponent, AddressInputComponent],
+            declarations: [
+                TestHostComponent,
+                AddAddressDialogComponent,
+                AddressInputComponent
+            ],
             providers: [
-                TestProviders.MockMatDialogRef(),
+                TestProviders.MockMatDialogRef(spyObj),
                 TestProviders.MockRaidenConfigProvider(),
                 TestProviders.HammerJSProvider(),
                 SharedService,
@@ -38,12 +63,33 @@ describe('AddAddressDialogComponent', () => {
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(AddAddressDialogComponent);
+        fixture = TestBed.createComponent(TestHostComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should return the values when the user presses the confirm button', function() {
+        const element = getDebugElement();
+        mockInput(
+            element,
+            `input[placeholder="Address"]`,
+            '0x504300C525CbE91Adb3FE0944Fe1f56f5162C75C'
+        );
+        mockInput(element, `input[placeholder="Label"]`, 'Test Node');
+
+        fixture.detectChanges();
+
+        clickElement(fixture.debugElement, '#addresses-save');
+
+        fixture.detectChanges();
+
+        expect(spy.calls.argsFor(0)[0]).toEqual({
+            address: '0x504300C525CbE91Adb3FE0944Fe1f56f5162C75C',
+            label: 'Test Node'
+        });
     });
 });
