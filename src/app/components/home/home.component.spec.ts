34cde6c827d41b684a62a3e8bce48f4db34a1739
taleldayekh <mail@talel.se>
2019-09-23 11:25:02 +0200

    Changes to text and fixed failing test

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HomeComponent } from './home.component';
 
@@ -8,6 +9,7 @@ describe('HomeComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
+            imports: [RouterTestingModule.withRoutes([])],
             declarations: [HomeComponent]
         }).compileComponents();
     }));
