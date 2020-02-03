526aef4f37fdd356f5f92d7ee74ccf659363a300
manuelwedler <manuel.wedler@googlemail.com>
2020-02-03 10:21:20 +0100

    Adds tests for utility service

--- src/app/services/utility.service.spec.ts
+++ src/app/services/utility.service.spec.ts
@@ -3,10 +3,23 @@ import { TestBed } from '@angular/core/testing';
 import { UtilityService } from './utility.service';
 
 describe('UtilityService', () => {
+    let service: UtilityService;
+
     beforeEach(() => TestBed.configureTestingModule({}));
 
+    beforeEach(() => {
+        service = TestBed.get(UtilityService);
+    });
+
     it('should be created', () => {
-        const service: UtilityService = TestBed.get(UtilityService);
         expect(service).toBeTruthy();
+    });
+
+    it('should register a new global click with target', () => {
+        const element = document.createElement('div');
+        service.globalClickTarget$.subscribe(target => {
+            expect(target).toEqual(element);
+        });
+        service.newGlobalClick(element);
     });
 });
