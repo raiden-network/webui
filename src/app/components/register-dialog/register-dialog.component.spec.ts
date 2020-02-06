7a6a865172ecfdf8da32b567cb1935d95f4f8315
manuelwedler <manuel.wedler@googlemail.com>
2020-02-06 18:05:26 +0100

    Adds tests for register dialog component

--- src/app/components/register-dialog/register-dialog.component.spec.ts
+++ src/app/components/register-dialog/register-dialog.component.spec.ts
@@ -6,19 +6,30 @@ import { MaterialComponentsModule } from '../../modules/material-components/mate
 import { AddressInputComponent } from '../address-input/address-input.component';
 import { RegisterDialogComponent } from './register-dialog.component';
 import { TestProviders } from '../../../testing/test-providers';
+import { RaidenDialogComponent } from '../raiden-dialog/raiden-dialog.component';
+import { createAddress } from '../../../testing/test-data';
+import { mockInput, clickElement } from '../../../testing/interaction-helper';
+import { By } from '@angular/platform-browser';
 
 describe('RegisterDialogComponent', () => {
     let component: RegisterDialogComponent;
     let fixture: ComponentFixture<RegisterDialogComponent>;
 
+    const addressInput = createAddress();
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [RegisterDialogComponent, AddressInputComponent],
+            declarations: [
+                RegisterDialogComponent,
+                AddressInputComponent,
+                RaidenDialogComponent
+            ],
             providers: [
                 TestProviders.MockMatDialogData(),
                 TestProviders.MockMatDialogRef({ close: () => {} }),
                 TestProviders.MockRaidenConfigProvider(),
-                TestProviders.AddressBookStubProvider()
+                TestProviders.AddressBookStubProvider(),
+                TestProviders.HammerJSProvider()
             ],
             imports: [
                 MaterialComponentsModule,
@@ -37,5 +48,47 @@ describe('RegisterDialogComponent', () => {
 
     it('should be created', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should close the dialog with the result when accept button is clicked', () => {
+        mockInput(
+            fixture.debugElement.query(By.directive(AddressInputComponent)),
+            'input',
+            addressInput
+        );
+        fixture.detectChanges();
+
+        // @ts-ignore
+        const closeSpy = spyOn(component.dialogRef, 'close');
+        clickElement(fixture.debugElement, '#accept');
+        fixture.detectChanges();
+
+        expect(closeSpy).toHaveBeenCalledTimes(1);
+        expect(closeSpy).toHaveBeenCalledWith(addressInput);
+    });
+
+    it('should close the dialog with no result when cancel button is clicked', () => {
+        mockInput(
+            fixture.debugElement.query(By.directive(AddressInputComponent)),
+            'input',
+            addressInput
+        );
+        fixture.detectChanges();
+
+        // @ts-ignore
+        const closeSpy = spyOn(component.dialogRef, 'close');
+        clickElement(fixture.debugElement, '#cancel');
+        fixture.detectChanges();
+
+        expect(closeSpy).toHaveBeenCalledTimes(1);
+        expect(closeSpy).toHaveBeenCalledWith();
+    });
+
+    it('should not submit the dialog by enter if the form is invalid', () => {
+        // @ts-ignore
+        const close = spyOn(component.dialogRef, 'close');
+        const dialog = fixture.debugElement.query(By.css('.dialog'));
+        dialog.triggerEventHandler('keyup.enter', {});
+        expect(close).toHaveBeenCalledTimes(0);
     });
 });
