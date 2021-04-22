11babaeb83050b556a2ec06f158e49ca4698e8d8
manuelwedler <manuel.wedler@googlemail.com>
2021-04-22 11:34:32 +0200

    Bump version: 1.1.1 → 1.2.0

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ with open('README.md', encoding='utf-8') as readme_file:
 
 history = ''
 
-version = '1.1.1'  # Do not edit: this is maintained by bumpversion (see .bumpversion.cfg)
+version = '1.2.0'  # Do not edit: this is maintained by bumpversion (see .bumpversion.cfg)
 
 setup(
     name='raiden-webui',
