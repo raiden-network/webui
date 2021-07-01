d0ecdcae33e65d52909285cdcc77f6528c45781a
manuelwedler <manuel.wedler@googlemail.com>
2021-07-01 15:00:05 +0200

    Update webpack config and polyfills to get web3 with webpack v5 working

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-    node: {
-        crypto: true,
-        http: true,
-        https: true,
-        os: true,
-        stream: true,
+    resolve: {
+        fallback: {
+            crypto: require.resolve('crypto-browserify'),
+            http: require.resolve('stream-http'),
+            https: require.resolve('https-browserify'),
+            os: require.resolve('os-browserify/browser'),
+            stream: require.resolve('stream-browserify'),
+            assert: require.resolve('assert/'),
+        },
     },
 };
